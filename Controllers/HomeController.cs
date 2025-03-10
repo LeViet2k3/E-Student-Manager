27815c4cf773f5a5a7d30e93cc04e42412f3c2e5
Le Viet <viet.gm.2k3@gmail.com>
2025-03-10 22:46:20 +0700

    - update home page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,14 +4,47 @@ namespace StudentApp.Controllers;
 
 public class HomeController : Controller
 {
-    public HomeController() { }
-
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+    public IActionResult StudentInfo()
+    {
+        return View();
+    }
+
+    public IActionResult StudyPlan()
+    {
+        return View();
+    }
+
+    public IActionResult RegisterSubject()
+    {
+        return View();
+    }
+
+    public IActionResult ExamSchedule()
+    {
+        return View();
+    }
+
+    public IActionResult StudyResult()
+    {
+        return View();
+    }
+
+    public IActionResult EvaluateSubject()
+    {
+        return View();
+    }
+
+    public IActionResult Tuition()
+    {
+        return View();
+    }
+
+    public IActionResult Timetable()
     {
         return View();
     }
