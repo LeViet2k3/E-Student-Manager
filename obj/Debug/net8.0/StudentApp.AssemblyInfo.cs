714be087ef1d5677dcc5c7a6ac401683cee5df01
Le Viet <viet.gm.2k3@gmail.com>
2025-04-23 22:58:17 +0700

    + complete the interface of the functions

--- obj/Debug/net8.0/StudentApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c33024f7a3fdb5a3d27183cefd09926dc1a3358d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a6f2368921e37448ec671c634f0f787c155c269")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
